97fdab6aa32e662577c4ec70840fd25a3d849805
demmer <jochen.demmer@peakwork.com>
2015-11-25 09:42:31 +0100

    multiple optimizations, current state not thorougly tested though.

--- autovpn.bash
+++ autovpn.bash
@@ -2,53 +2,68 @@
 # autovpn
 # read and understand
 # Jochen Demmer
-checkinterval=60
-checkifs=( 'enp0s25', 'wlp3s0' )
-if [ -z $1 ]; then
+#
+# define from here
+# if ping is not successfull, sleep for seconds given:
+pingsleeptimer=60
+# if network interface is not up, sleep for seconds given:
+ifupsleeptimer=10
+# give the network interfaces that shall be checked:
+checkifs=( 'enp0s25' 'wlp3s0' )
+
+# do not change from here
+function tellhow {
 	echo "First parameter is path to vpn config."
+	echo "Second parameter is a host in your destination network that is up and reachable via ICMP type 8, i.e. ping."
+}
+
+function testing {
+	echo "`$0`"
+}
+if [ -z $1 ]; then
+	tellhow
 	exit 1
 fi
 if [ -z $2 ]; then
-	echo "Second parameter is host that shall be pinged in order to check if connected to this remote net."
+	tellhow
 	exit 1
 fi
 
-echo "`date` $0 initializing script"
-echo "`date` $0 waiting for network to come up"
-
+echo "`date` $0 Initializing script."
+echo "`date` $0 Waiting for network to come up"
 
 while [ true ]; do
 	x=0
 	for i in "${checkifs[@]}"; do
-		if [ `ip a s $i |grep state |grep -c UP` -gt 0 ]; then
+#		if [ `ip a s $i |grep state |grep -c UP` -gt 0 ]; then
+		if [ `cat /sys/class/net/$i/operstate |grep -c up` -gt 0 ]; then
 			echo "`date` $0 interface $i seems to be up."
 			((x++))
 		else
 			echo "`date` $0 interface $i seems to be down."
+			sleep 1
 		fi
 	done
 	if [ $x -gt 0 ]; then
 		if [ `ping -c 5 8.8.8.8 | grep -c "bytes from"` -gt 3 ]; then
-			echo "`date` $0 there seem to be internet available"
+			echo "`date` $0 there seem to be internet available."
 			break
 		else
-			echo "`date` $0 there is no internet. I'll keep trying."
-			sleep 10
+			echo "`date` $0 there is no internet. I'll keep trying, interval $ifupsleeptimer seconds."
+			sleep $ifupsleeptimer
 		fi
 	fi
 done
 
-
-
 while  [ true ]; do
 	if [ `ping -c 5 $2 |grep -c "bytes from"` -gt 3 ]; then
 		# echo "ping to $2 at least 4 out of 5 times ok"
-		echo "`date` $0 Already connected to ha.home"
+		echo "`date` $0 Already connected to destination network."
 	else
 		# echo "ping to $2 at least 2 out of 5 times not ok"
-		echo "`date` $0 starting vpn connection"
+		echo "`date` $0 Starting vpn connection"
 		openvpn --config $1
 	fi
-	echo "`date` $0 sleeping for $checkinterval seconds"
-	sleep $checkinterval
+	echo "`date` $0 Remote network not reachable. I'll keep trying, interval $pingsleeptimer seconds."
+	sleep $pingsleeptimer
 done
